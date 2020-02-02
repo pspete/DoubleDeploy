0f02cc15730290626ee56f897b91d64664008571
Pete Maan <pete.maan+github@gmail.com>
2020-02-02 23:41:51 +0000

    Update deploy.ps1

--- build/deploy.ps1
+++ build/deploy.ps1
@@ -24,17 +24,17 @@ if (-not ($ENV:APPVEYOR_PULL_REQUEST_NUMBER)) {
 		git config --global user.name "Pete Maan"
 
 		Write-Host "Checking out branch: $($ENV:APPVEYOR_REPO_BRANCH)"
-		git checkout -q $($ENV:APPVEYOR_REPO_BRANCH)
+		git checkout -q master
 
 		Write-Host "Staging Change"
-		git add $(Join-Path "$env:APPVEYOR_PROJECT_NAME" "$env:APPVEYOR_PROJECT_NAME.psd1")
+		git add $(Join-Path "$env:APPVEYOR_BUILD_FOLDER" "$env:APPVEYOR_PROJECT_NAME" "$env:APPVEYOR_PROJECT_NAME.psd1")
 
 		Write-Host "Status"
 		git status
 		Write-Host "Commit"
 		git commit -s -m "Update Version"
 		Write-Host "Push"
-		git push --porcelain origin "$($ENV:APPVEYOR_REPO_BRANCH)" --verbose
+		git push --porcelain origin master --verbose
 
 		Write-Host "$($env:APPVEYOR_PROJECT_NAME) updated version pushed to GitHub." -ForegroundColor Cyan
 
