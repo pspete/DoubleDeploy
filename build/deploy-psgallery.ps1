72f65c5baa12d69656fba478d39be156febca988
Pete Maan <pete.maan+github@gmail.com>
2020-02-04 20:38:02 +0000

    Create deploy-psgallery.ps1

---
+++ build/deploy-psgallery.ps1
@@ -0,0 +1,87 @@
+<#---------------------------------
+Auto-publish changes to master branch as a new module version in the PSGallery.
+- Only publish if build version is greater than 1.0
+- Skip Auto-publish with specific commit message
+---------------------------------#>
+
+
+if (-not ($ENV:APPVEYOR_PULL_REQUEST_NUMBER)) {
+
+	Write-Host "Deploy Process: PowerShell Gallery" -ForegroundColor Yellow
+
+	<#---------------------------------#>
+	<# If Not a PR                     #>
+	<#---------------------------------#>
+	If ($ENV:APPVEYOR_REPO_BRANCH -eq 'master') {
+
+		<# Master Branch     #>
+
+		If ($env:APPVEYOR_BUILD_VERSION -ge "1.0.0") {
+
+			<# Version 1.0+     #>
+
+			If ($ENV:APPVEYOR_REPO_COMMIT_MESSAGE -eq "Manual Deployment") {
+
+				<# Manual Deploy to PSGallery #>
+				Write-Host "Finished testing of branch: $env:APPVEYOR_REPO_BRANCH"
+				Write-Host "Manual Deployment to PSGallery Required"
+				Write-Host "Exiting"
+				exit;
+
+			}
+			Else {
+
+				<#---------------------------------#
+		 		# Publish to PS Gallery            #
+		 		#----------------------------------#>
+
+				Try {
+
+					Write-Host 'Publish to Powershell Gallery...'
+
+					$ModulePath = Join-Path $env:APPVEYOR_BUILD_FOLDER $env:APPVEYOR_PROJECT_NAME
+
+					Write-Host "Publishing: $ModulePath"
+
+					Publish-Module -Path $ModulePath -NuGetApiKey $($env:psgallery_key) -SkipAutomaticTags -Confirm:$false -ErrorAction Stop -Force
+
+					Write-Host "$($env:APPVEYOR_PROJECT_NAME) published." -ForegroundColor Cyan
+
+				}
+				Catch {
+
+					Write-Warning "Publish Failed."
+					throw $_
+
+				}
+				Finally {
+					exit;
+				}
+
+			}
+
+		}
+		Else {
+
+			<# Less Than version 1.0 - No Deployment   #>
+			Write-Host "Nothing to Deploy - Exiting"
+			exit;
+
+		}
+
+	}
+	Else {
+
+		<# Not Master Branch - No Deployment      #>
+
+		Write-Host "Finished testing of branch: $env:APPVEYOR_REPO_BRANCH - Exiting"
+		exit;
+
+	}
+
+}
+Else {
+
+	Write-Host "Skipping Deploy Process: PowerShell Gallery" -ForegroundColor Yellow
+
+}
