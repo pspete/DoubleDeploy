f2caf82734e890d6bc72ee23b8e2737e7bcf2ec7
Pete Maan <pete.maan+github@gmail.com>
2020-02-05 00:27:08 +0000

    Update test.ps1

--- build/test.ps1
+++ build/test.ps1
@@ -1,38 +1,32 @@
 #---------------------------------#
 # Header                          #
 #---------------------------------#
-Write-Host "Testing: PSVersion $($PSVersionTable.PSVersion)" -ForegroundColor Yellow
-
-$Dir = $(Split-Path $env:APPVEYOR_BUILD_FOLDER -Parent)
-$TestsResults = $(Join-Path $Dir "TestsResults.xml")
-$CodeCoverage = $(Join-Path $Dir "coverage.json")
-$OutFile = $(Join-Path $Dir "codecov.sh")
+Write-Host "Testing:" -ForegroundColor Yellow
+Write-Host "Current working directory: $pwd"
 
 #---------------------------------#
 # Run Pester Tests                #
 #---------------------------------#
 $files = Get-ChildItem $(Join-Path $ENV:APPVEYOR_BUILD_FOLDER $env:APPVEYOR_PROJECT_NAME) -Include *.ps1 -Recurse
 
-$res = Invoke-Pester -Path ".\Tests" -OutputFormat NUnitXml -OutputFile $TestsResults -CodeCoverage $files -PassThru
+$res = Invoke-Pester -Path ".\Tests" -OutputFormat NUnitXml -OutputFile ..\TestsResults.xml -CodeCoverage $files -PassThru
 
 Write-Host 'Uploading Test Results'
-(New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", $TestsResults)
+(New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", $(Resolve-Path ..\TestsResults.xml))
 
 if ($env:APPVEYOR_REPO_COMMIT_AUTHOR -eq "Pete Maan") {
 
 	Write-Host 'Formating Code Coverage'
 	$coverage = Format-Coverage -PesterResults $res -CoverallsApiToken $($env:coveralls_key) -BranchName $($env:APPVEYOR_REPO_BRANCH)
 
-	$coverageJson = Export-CodeCovIoJson -CodeCoverage $res.CodeCoverage -RepoRoot $pwd -Path $CodeCoverage
+	Export-CodeCovIoJson -CodeCoverage $res.CodeCoverage -RepoRoot $pwd -Path $(Resolve-Path ..\coverage.json)
 
 	Write-Host 'Publishing Code Coverage'
-
 	Publish-Coverage -Coverage $coverage
 
-	Invoke-WebRequest -Uri 'https://codecov.io/bash' -OutFile $OutFile
+	Invoke-WebRequest -Uri 'https://codecov.io/bash' -OutFile $(Resolve-Path ..\codecov.sh)
 
-	bash $OutFile -f $coverageJson
-
+	bash $(Resolve-Path ..\codecov.sh) -f $(Resolve-Path ..\coverage.json)
 
 }
 #---------------------------------#
