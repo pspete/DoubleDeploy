cbe3814ede1438593acd5e03fded62e51a57af48
Pete Maan <pete.maan+github@gmail.com>
2020-02-05 00:54:23 +0000

    Update test.ps1

--- build/test.ps1
+++ build/test.ps1
@@ -9,24 +9,29 @@ Write-Host "Current working directory: $pwd"
 #---------------------------------#
 $files = Get-ChildItem $(Join-Path $ENV:APPVEYOR_BUILD_FOLDER $env:APPVEYOR_PROJECT_NAME) -Include *.ps1 -Recurse
 
-$res = Invoke-Pester -Path ".\Tests" -OutputFormat NUnitXml -OutputFile ..\TestsResults.xml -CodeCoverage $files -PassThru
+$res = Invoke-Pester -Path ".\Tests" -OutputFormat NUnitXml -OutputFile .\TestsResults.xml -CodeCoverage $files -PassThru
 
 Write-Host 'Uploading Test Results'
-(New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", $(Resolve-Path ..\TestsResults.xml))
+(New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", $(Resolve-Path .\TestsResults.xml))
+
+Remove-Item -Path $(Resolve-Path .\TestsResults.xml) -Force
 
 if ($env:APPVEYOR_REPO_COMMIT_AUTHOR -eq "Pete Maan") {
 
 	Write-Host 'Formating Code Coverage'
 	$coverage = Format-Coverage -PesterResults $res -CoverallsApiToken $($env:coveralls_key) -BranchName $($env:APPVEYOR_REPO_BRANCH)
 
-	Export-CodeCovIoJson -CodeCoverage $res.CodeCoverage -RepoRoot $pwd -Path ..\coverage.json
+	Export-CodeCovIoJson -CodeCoverage $res.CodeCoverage -RepoRoot $pwd -Path .\coverage.json
 
 	Write-Host 'Publishing Code Coverage'
 	Publish-Coverage -Coverage $coverage
 
-	Invoke-WebRequest -Uri 'https://codecov.io/bash' -OutFile ..\codecov.sh
+	Invoke-WebRequest -Uri 'https://codecov.io/bash' -OutFile .\codecov.sh
 
-	bash ..\codecov.sh -f ..\coverage.json
+	bash .\codecov.sh -f .\coverage.json
+
+	Remove-Item -Path $(Resolve-Path .\coverage.json) -Force
+	Remove-Item -Path $(Resolve-Path .\codecov.sh) -Force
 
 }
 #---------------------------------#
