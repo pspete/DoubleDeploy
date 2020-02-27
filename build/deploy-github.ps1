1ac50aa215780a28eb1a4a9eb2da22959c84648b
Pete Maan <pete.maan+github@gmail.com>
2020-02-27 21:02:06 +0000

    Update deploy-github.ps1
    Changed to only push updated version humber in manifest file on master branch, not for every build on every branch

--- build/deploy-github.ps1
+++ build/deploy-github.ps1
@@ -5,49 +5,48 @@ Update version number on GitHub to match build version
 
 if (-not ($ENV:APPVEYOR_PULL_REQUEST_NUMBER)) {
 
-	Write-Host "Deploy Process: GitHub Repository" -ForegroundColor Yellow
-
-	<#---------------------------------#>
-	<# If Not a PR                     #>
-	<# Push psd1 file to ORIGIN Branch #>
-	<#---------------------------------#>
-
-	Try {
-
-		Write-Host "Push Updated $($env:APPVEYOR_PROJECT_NAME).psd1 to GitHub..." -ForegroundColor Yellow
-
-		git config --global core.safecrlf false
-		git config --global credential.helper store
-		Add-Content "$HOME\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"
-		git config --global user.email "$($env:github_email)"
-		git config --global user.name "Pete Maan"
-
-		git checkout -q $($ENV:APPVEYOR_REPO_BRANCH)
-
-		git add $(Join-Path -Path (Join-Path -Path "$env:APPVEYOR_BUILD_FOLDER" -ChildPath "$env:APPVEYOR_PROJECT_NAME") -ChildPath "$env:APPVEYOR_PROJECT_NAME.psd1")
-
-		git status
-
-		git commit -s -m "Update Version: $($env:APPVEYOR_BUILD_VERSION)"
-
-		git push --porcelain origin $($ENV:APPVEYOR_REPO_BRANCH)
-
-		Write-Host "$($env:APPVEYOR_PROJECT_NAME) version $($env:APPVEYOR_BUILD_VERSION) pushed to GitHub." -ForegroundColor Cyan
-
-	}
-
-	Catch {
-
-		Write-Host "Push to GitHub failed." -ForegroundColor Red
-		throw $_
-
-	}
-
-	Write-Host "Deploy Process: GitHub Release" -ForegroundColor Yellow
-
 	If ($ENV:APPVEYOR_REPO_BRANCH -eq 'master') {
 
 		<# Master Branch #>
+		Write-Host "Deploy Process: GitHub Repository" -ForegroundColor Yellow
+
+		<#---------------------------------#>
+		<# If Not a PR                     #>
+		<# Push psd1 file to ORIGIN Branch #>
+		<#---------------------------------#>
+
+		Try {
+
+			Write-Host "Push Updated $($env:APPVEYOR_PROJECT_NAME).psd1 to GitHub..." -ForegroundColor Yellow
+
+			git config --global core.safecrlf false
+			git config --global credential.helper store
+			Add-Content "$HOME\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"
+			git config --global user.email "$($env:github_email)"
+			git config --global user.name "Pete Maan"
+
+			git checkout -q $($ENV:APPVEYOR_REPO_BRANCH)
+
+			git add $(Join-Path -Path (Join-Path -Path "$env:APPVEYOR_BUILD_FOLDER" -ChildPath "$env:APPVEYOR_PROJECT_NAME") -ChildPath "$env:APPVEYOR_PROJECT_NAME.psd1")
+
+			git status
+
+			git commit -s -m "Update Version: $($env:APPVEYOR_BUILD_VERSION)"
+
+			git push --porcelain origin $($ENV:APPVEYOR_REPO_BRANCH)
+
+			Write-Host "$($env:APPVEYOR_PROJECT_NAME) version $($env:APPVEYOR_BUILD_VERSION) pushed to GitHub." -ForegroundColor Cyan
+
+		}
+
+		Catch {
+
+			Write-Host "Push to GitHub failed." -ForegroundColor Red
+			throw $_
+
+		}
+
+		Write-Host "Deploy Process: GitHub Release" -ForegroundColor Yellow
 
 		If ($env:APPVEYOR_BUILD_VERSION -ge "1.0.0") {
 
